3315fb2e9b78f67f985d1ac6a248777e89986fef
David Oliveira <d.oliveira@prozone.org>
2014-06-29 23:38:55 +0100

    Proxy support. Support for route options. Support for method specification. New version - 0.1.0

--- test/test.js
+++ test/test.js
@@ -9,13 +9,20 @@ spritz.on('/',function(req,res){
 	res.writeHead(200,{});
 	res.end('Aperol o Campari?');
 });
-spritz.on(/x/,function(req,res){
-	res.writeHead(200,{});
-	res.end('Soda?');
+spritz.on(/^\/x/,function(req,res){
+	spritz.text(req,res,'Soda?');
 });
 spritz.on(/passwd/,function(req,res){
 	spritz.staticfile(req,res,"/etc/passwd");
 });
 spritz.on(/json/,function(req,res){
 	spritz.json(req,res,{some:"json",other:1});
+});
+spritz.on('/post',{method:"POST"},function(req,res){
+	console.log("GOT a POST request: ",req.headers);
+	spritz.json(req,res,{postData:req.POSTargs});
+});
+spritz.on('/npm/',{dontReadPOSTData:true},function(req,res){
+	spritz.proxy(req,res,"https://www.npmjs.org/");
+//	spritz.proxy(req,res,"127.0.0.1",9999,{proto:"http",timeout: 2000});
 });
