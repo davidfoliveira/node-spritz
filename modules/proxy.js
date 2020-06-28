7c615ee734c1b99f957fe2fa526f02ff38f07986
David Oliveira <david@Davids-MacBook.local>
2020-06-28 10:40:14 +0100

    Fix issues with proxy outputFilter

--- modules/proxy.js
+++ modules/proxy.js
@@ -18,7 +18,7 @@ exports.init = function(){
         self = this;
 
     // Proxy the request
-    self.proxy = function(req,res,hostOrURL,port,opts,callback){
+    self.proxy = function(req, res, hostOrURL, port, opts, callback){
 
         var
             args = Array.prototype.slice.call(arguments, 0),
@@ -45,7 +45,7 @@ exports.init = function(){
             port:    port,
             path:    url,
             headers: req.headers || {}
-        },opts||{},true);
+        }, opts||{}, true);
 
         // Trying to proxy a POST request with already read POST data ?
         if ( req.method == "POST" && req._readPOSTData ) {
@@ -62,13 +62,13 @@ exports.init = function(){
         // Host:port
         else if ( hostOrURL.match(/:(\d+)$/) ) {
             _opts.port = parseInt(RegExp.$1);
-            _opts.host = hostOrURL.replace(/:.*$/,"");
+            _opts.host = hostOrURL.replace(/:.*$/, " ");
             _opts.headers.host = _opts.host;
         }
         // URL
         else if ( hostOrURL.match(/^https?:\/\//) ) {
             var u = require('url').parse(hostOrURL);
-            _opts.proto = u.protocol.replace(/:.*$/,"");
+            _opts.proto = u.protocol.replace(/:.*$/, "");
             _opts.host = u.hostname;
             _opts.headers.host = u.hostname;
             _opts.port = u.port;
@@ -78,6 +78,10 @@ exports.init = function(){
         // No port ? defaults to the default protocol port
         if ( !_opts.port )
             _opts.port = (_opts.proto == "https" ? 443 : 80);
+
+        // If we have an output filter, make sure we delete the 'accept-encoding' header to not get encoded data
+        if (opts.outputFilter)
+            delete req.headers['accept-encoding'];
 
         var
             proto = (_opts.proto == "https") ? https : http,
@@ -96,63 +100,67 @@ exports.init = function(){
                 fired = true;
                 if ( _opts.onTimeout )
                     return _opts.onTimeout();
-                return self._writeHead(self,req,res,502,{'Content-type':'text/plain; charset=UTF-8'},function(){
-                    return self._writeData(self,req,res,'502 - Gateway timeout :-(',true,function(){});
+                return self._writeHead(self, req, res, 502, {'Content-type':'text/plain; charset=UTF-8'}, function(){
+                    return self._writeData(self, req, res, '502 - Gateway timeout :-(', true, function(){});
                 });
-            },_opts.timeout);
+            }, _opts.timeout);
         }
 
         // On response arrive
-        preq.on('response',function(pres){
+        preq.on('response', function(pres){
             if ( fired )
                 return;
             if ( timeout )
                 clearTimeout(timeout);
-            return self._writeHead(self,req,res,pres.statusCode,pres.headers,function(){
-                if ( typeof opts.outputFilter == "function" ) {
-                    var allData = null;
-                    pres.on('data',function(data){
-                        var newB = new Buffer(((allData != null)?allData.length:0)+data.length);
-                        if ( allData != null )
-                            allData.copy(newB,0,0,allData.length);
-                        data.copy(newB,(allData != null)?allData.length:0,0,data.length);
-                        allData = newB;
-                    });
-                    pres.on('end',function(){
-                        var d = opts.outputFilter(allData,req,res,preq,pres);
-                        if ( d == null )
-                            d = allData;
-                        docSize = d.length;
-                        return self._writeData(self,req,res,d,true,function(){
+
+            if ( typeof opts.outputFilter == "function" ) {
+                var allData = null;
+                pres.on('data', function(data){
+                    var newB = new Buffer(((allData != null) ? allData.length : 0)+data.length);
+                    if ( allData != null )
+                        allData.copy(newB, 0, 0, allData.length);
+                    data.copy(newB, ((allData != null) ? allData.length : 0), 0, data.length);
+                    allData = newB;
+                });
+                pres.on('end', function(){
+                    var d = opts.outputFilter(allData, req, res, preq, pres);
+                    if ( d == null )
+                        d = allData;
+
+                    pres.headers['content-length'] = d.length;
+                    return self._writeHead(self, req, res, res.statusCode, pres.headers, function(){
+                        return self._writeData(self, req, res, d, true, function(){
                             // Run the callback
                             if ( callback )
-                                callback(null,true);
+                                callback(null, true);
 
                             // Log
-                            return _access_log(req,res,pres.headers['content-length']||docSize||'??');
+                            return self._access_log(req, res, pres.headers['content-length']||'??');
                         });
                     });
-                }
-                else {
+                });
+            }
+            else {
+                return self._writeHead(self, req, res, pres.statusCode, pres.headers, function(){
                     var pr = pres.pipe(res);
-                    pr.on('end',function(){
+                    pr.on('end', function(){
                         // Run the callback
                         if ( callback )
-                            callback(null,true);
+                            callback(null, true);
 
                         // Log
-                        _access_log(req,res,pres.headers['content-length']||docSize||'??');
+                        self._access_log(req, res, pres.headers['content-length']||'??');
                     });
-                }
-            });
+                });
+            }
         });
-        preq.on('error',function(e){
+        preq.on('error', function(e){
             if ( _opts.onError )
                 return _opts.onError(e);
-            return self._writeHead(self,req,res,503,{'content-type':'text/plain; charset=UTF-8'},function(){
-                return self._writeData(self,req,res,'503 - Gateway error: '+e.toString(),true,function(){
+            return self._writeHead(self, req, res, 503, {'content-type':'text/plain; charset=UTF-8'}, function(){
+                return self._writeData(self, req, res, '503 - Gateway error: '+e.toString(), true, function(){
                     preq.abort();
-                    return self._access_log(req,res,19);
+                    return self._access_log(req, res, 19);
                 });
             });
         });
